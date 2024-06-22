12eefb3d98bb52e664161471d1eb36adef265fce
Prachi Parihar <prachisingh30aabb@gmail.com>
2024-06-22 12:43:28 +0530

    Add Files

--- 01basics/src/App.js
+++ 01basics/src/App.js
@@ -1,10 +1,12 @@
-
+import Chai from "./Chai";
 
 function App() {
   const name="prachi";
   return (
-    
+    <>
+    <Chai/>
     <h1>Hello {name}</h1>
+    </>
 
   );
 }
---
+++ 01basics/src/Chai.js
@@ -0,0 +1,7 @@
+function Chai (){
+    return (
+        <h1>Hello There i am a chai component</h1>
+    )
+}
+
+export default Chai; 
