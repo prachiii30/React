9ddbbfc4b44df4167a776d6a52ec8aa5575a275d
Prachi Parihar <prachisingh30aabb@gmail.com>
2024-06-27 12:38:26 +0530

    Add Files

--- 05_pwdgenerator/src/App.jsx
+++ 05_pwdgenerator/src/App.jsx
@@ -1,47 +1,80 @@
-import { useState,useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
+import { useState,useCallback ,useEffect} from 'react'
 function App() {
+  const[length,setLength]=useState(8);
+  const[numallowed,setNumallowed]=useState(false);
+  const[charallowed,setCharallowed]=useState(false);
+  const [password,setPassword]=useState()
 
-  const [len,setLen]=useState(6)
-  const [num,setNum]=useState(false)
-  const [char,setChar]=useState(false)
-  const [pwd,setPwd]=useState("")
-
-  const pwdGenarator=useCallback(()=>{
+ const passwordgen=useCallback(()=>{
     let pass=""
     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(num) str+="0123456789"
-    if(char) str+="!@#$%^&*()?/"
+    
+    if(numallowed) str+="0123456789"
+    if(charallowed) str+=".;./;@#!}{?"
 
-    for(let i=1;i<=len;i++){
+    for(let i=1;i<length;i++){
       let char=Math.floor(Math.random()*str.length+1)
-      let pass=str.charAt(char)
+        pass=pass+str.charAt(char)
     }
-    setPwd(pass)
-  },[len,num,char,setPwd])
+    setPassword(pass)
+ },[length,charallowed,numallowed,setPassword])
+ useEffect(()=>{
+  passwordgen()}
+  ,[length,numallowed,charallowed,passwordgen])
+ 
+
   
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-500'> 
-      <div className='flex shadow rounded-lg overflow-hidden '>
-        <input type="text"
-        value={pwd} 
+      <div className='w-full max-w-4xl mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-500'> 
+      <h1 className="text-4xl text-center text-black">Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden m-5'>
+        <input 
+        type="text"
+        value={password}
         className='outline-none w-full py-1 px-3'
         placeholder='password'
-        readOnly/>
+        readOnly
+        />
         <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
       </div>
 
       <div className='flex text-sm gap-x-2'>
         <div className='flax items-center gap-x-1 '>
-          <input type="range" 
-          name="" id=""/>
+          <input 
+          type="range" 
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer m-2'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label className='text-xl text-gray-700 pl-2'>Length:({length})</label>
 
         </div>
+        <div>
+          <input type="checkbox" 
+          defaultChecked={numallowed}
+          id="numip"
+          className='m-3'
+          onChange={()=>{
+            setNumallowed((prev)=>!prev)
+          }}
+          />
+          <label className='text-xl'>Number</label>
+        </div>
+        <div>
+          <input type="checkbox" 
+          defaultChecked={charallowed}
+          id="numip"
+          className='m-3'
+          onChange={()=>{
+            setCharallowed((prev)=>!prev)
+          }}
+          />
+          <label className='text-xl'>Character</label>
+
+          </div>
       </div>
       </div>
 
